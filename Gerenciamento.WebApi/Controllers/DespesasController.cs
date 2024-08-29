08b43d49aa92569931d0835e51789220ffb4d499
chagasduarte <chagasduarte.rdg@gmail.com>
2024-08-29 19:53:55 -0300

    verificação se é fixa

--- Gerenciamento.WebApi/Controllers/DespesasController.cs
+++ Gerenciamento.WebApi/Controllers/DespesasController.cs
@@ -30,7 +30,7 @@ namespace Gerenciamento.WebApi.Controllers
         public async Task<ActionResult<IEnumerable<Despesa>>> GetDespesasPoMes(int mes)
         {
             return await _context.Despesas
-                .Where(x => x.MesCompra == mes)
+                .Where(x => x.MesCompra == mes || x.IsFixa)
                 .ToListAsync();
         }
         [HttpGet("Fixas")]
